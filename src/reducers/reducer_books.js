c12ea4cb53b5c7678343491b4fe3225e6dad6c2c
justbo <b.tsybulskyy@gmail.com>
2017-12-28 18:10:44 +0200

    start from begining

--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    { title: 'Javascript: The Food Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The white Tower' },
-    { title: 'Eloquent' }
-  ]
-}
